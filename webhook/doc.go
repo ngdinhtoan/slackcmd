096c34833efb7416c7d35ee97c802dbf54040513
Toan Nguyen <ngdinhtoan@gmail.com>
2015-06-26 23:23:33 +0700

    Added documentation for webhook package.

---
+++ webhook/doc.go
@@ -0,0 +1,41 @@
+// Package webhook provide some useful structs and functions
+// to send message to Slack via Incomming Webhook.
+/*
+Example send hello message to Slack via Incomming Webhook:
+
+    package main
+
+    import "github/ngdinhtoan/slackcmd/webhook"
+
+    func main() {
+        payload := webhook.NewPayload()
+        payload.Text = "Hello Slack"
+        payload.Username = "SlackCmd"
+
+        webhook.SendMessageToHook(
+            payload,
+            "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX")
+    }
+
+Send rich message by Message Attachments (see https://api.slack.com/docs/attachments):
+
+    package main
+
+    import "github/ngdinhtoan/slackcmd/webhook"
+
+    func main() {
+        payload := webhook.NewPayload()
+        payload.Text = "Hello Slack"
+        payload.Username = "SlackCmd"
+
+        attachment := webhook.NewAttachment("full message here", "title here", "http://title.url/here")
+        attachment.SetColor("#F00")
+
+        payload.AddAttachment(attachment)
+        webhook.SendMessageToHook(
+            payload,
+            "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX")
+    }
+
+*/
+package webhook
